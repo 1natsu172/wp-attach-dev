b4d524910451bd4ac74f58d56fbd6f24baa3adb0
z-ina-tt <z-inagaki@taitan.jp>
2017-04-10 19:49:08 +0900

    change dirSet feeling

--- configs/dirSets.js
+++ configs/dirSets.js
@@ -1,8 +1,8 @@
 //// ディレクトリ構成定義しましょう
 
 const DIRBASE = {
-  domain: 'wp-theme-devpack.dev', //サイトのドメインを適宜定義しましょう：http://ナシの接尾'/'ナシ
-  wordpressThemesFolder: './app/public/wp-content/themes/', // WPのthemesフォルダまでのパスを定義しましょう：ルートディレクトリ(gulpfileなどがあるディレクトリ)から相対パス始まりの接尾'/'アリ
+  domain: 'wp-theme-customize-devpack.dev', //サイトのドメインを適宜定義しましょう：http://ナシの接尾'/'ナシ
+  wordpressThemesFolder: '../app/public/wp-content/themes/', // WPのthemesフォルダまでのパスを定義しましょう：ルートディレクトリ(gulpfileなどがあるディレクトリ)から相対パス始まりの接尾'/'アリ
   developThemeName: 'twentyseventeen/', // 開発したいテーマのフォルダを指定しましょう：接尾'/'アリ
   buildFolder: 'build/', // テーマフォルダに配置する自前の開発用ソースのフォルダを指定しましょう。特別変更する必要なければデフォルトのままでOKです：接尾'/'アリ
   srcAssets: 'customizingAssets', // buildフォルダ内のアセットフォルダの名前を指定します。特別変更する必要なければデフォルトのままでOKです：接尾'/'ナシ
@@ -16,8 +16,8 @@ export const DIR = {
   wpThemes: DIRBASE.wordpressThemesFolder,
   devTheme: DIRBASE.wordpressThemesFolder + DIRBASE.developThemeName,
   src: {
-    base: DIRBASE.wordpressThemesFolder + DIRBASE.developThemeName + DIRBASE.buildFolder,
-    assets: DIRBASE.wordpressThemesFolder + DIRBASE.developThemeName + DIRBASE.buildFolder + DIRBASE.srcAssets
+    base: './' + DIRBASE.buildFolder,
+    assets: './' + DIRBASE.buildFolder + DIRBASE.srcAssets
   },
   dest: {
     base: DIRBASE.wordpressThemesFolder + DIRBASE.developThemeName,
