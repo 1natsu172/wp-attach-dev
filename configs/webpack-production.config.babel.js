597bd2d71838aa8210758eec770fd6966ab1c243
z-ina-tt <z-inagaki@taitan.jp>
2017-04-10 21:44:41 +0900

    add webpack-merge

--- configs/webpack-production.config.babel.js
+++ configs/webpack-production.config.babel.js
@@ -3,11 +3,12 @@
 import { DIR } from './dirSets.js'; // Directory config of project
 import path from 'path';
 import webpack from 'webpack';
+import webpackMerge from 'webpack-merge';
 import commonConfig from './webpack-common.config.babel.js';
 
 
 // js処理config(commonConfigとマージする)
-const jsConfig = Object.assign({}, commonConfig, {
+let jsConfig = webpackMerge(commonConfig, {
     entry: {
       scripts: DIR.src.assets+"/js/scripts.js"
     },
@@ -21,3 +22,6 @@ const jsConfig = Object.assign({}, commonConfig, {
 
 // 作成したconfig変数を使用する(複数configあるなら配列でmodule.exportsする)
 module.exports = jsConfig;
+
+// console.log(JSON.stringify(jsConfig, null, 2));
+// console.log(jsConfig);
