985cfc0c65896fd427c732f7e73245ca51bdcc19
1natsu <1natsummer+github@gmail.com>
2017-10-19 18:53:12 +0900

    httpProtocol adjust support

--- configs/dirSets.js
+++ configs/dirSets.js
@@ -8,14 +8,30 @@ let DIRBASE = {
   buildFolder: 'build', // 本開発キット同梱のビルドフォルダを指定しましょう。特別変更する必要なければデフォルトのままでOKです
   srcAssets: 'customizingAssets', // 本開発キット同梱のビルドフォルダ内にあるアセットフォルダの名前を指定します。特別変更する必要なければデフォルトのままでOKです。
   destAssets: 'customizedAssets', // テーマフォルダ内に吐き出されるコンパイル後のアセットフォルダの名前を指定しましょう。特別変更する必要なければデフォルトのままでOKです。
-}
+};
 
-DIRBASE.buildFolder = path.isAbsolute(DIRBASE.buildFolder) ? '.' + DIRBASE.buildFolder : './' + DIRBASE.buildFolder;
+function deleteFulfillString(string, regex) {
+  if (regex.test(string)) {
+    return string.replace(regex, "");
+  } else {
+    return arguments[0];
+  }
+};
+
+const regex_httpProtocol = /^(http.?:\/{2})/im;
+
+function adjustToRelativePath() {
+  return DIRBASE.buildFolder = path.isAbsolute(DIRBASE.buildFolder) ? '.' + DIRBASE.buildFolder : './' + DIRBASE.buildFolder;
+};
+
 
 // DIRBASEで設定したディレクトリ構成を元にディレクトリパスを定義します。
 // そして色んな場所でフォルダ構成を使えるように、モジュールエクスポートするやつです。
 export default function dirSets(pathString) {
-  const period = pathString || ''; // webpackは'./'はじまりのpathでないといけない、gulp(3.9.1)は'./'はじまりのpathだとダメなので引数で受け取れるようにする
+  const period = pathString || ''; // webpackは'./'形式の相対pathでないといけなく、gulp(3.9.1)は'./'形式の相対pathはNGなので引数で'./'を受け取れるようにする
+
+  adjustToRelativePath();
+  DIRBASE.domain = deleteFulfillString(DIRBASE.domain, regex_httpProtocol);
 
   return {
     domain: path.normalize(DIRBASE.domain),
